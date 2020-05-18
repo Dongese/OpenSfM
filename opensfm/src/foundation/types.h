ece7f4bab582df5266c7e7c680426f145621dc25
Yann Noutary <yann@mapillary.com>
2020-05-18 14:36:31 +0200

    feat: added basic eigen typedefs

---
+++ opensfm/src/foundation/types.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <Eigen/Eigen>
+
+template< class T >
+using Vec3T = Eigen::Matrix<T, 3, 1>;
+using Vec3d = Vec3T<double>;
+template< class T >
+using Vec2T = Eigen::Matrix<T, 2, 1>;
+using Vec2d = Vec2T<double>;
+template< class T >
+using VecXT = Eigen::Matrix<T, -1, 1>;
+using VecXd = VecXT<double>;
+
+template< class T >
+using Mat2T = Eigen::Matrix<T, 2, 2>;
+using Mat2d = Mat2T<double>;
+
+template< class T >
+using MatXT = Eigen::Matrix<T, -1, -1>;
+using MatXd = MatXT<double>;
